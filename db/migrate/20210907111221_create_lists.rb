2f912c8fdffd807fc3f5d37845027759c50dbf4d
EC2 Default User <ec2-user@ip-172-31-27-205.ec2.internal>
2021-09-07 11:35:52 +0000

    [finish]rails3

--- db/migrate/20210907111221_create_lists.rb
+++ db/migrate/20210907111221_create_lists.rb
@@ -1,7 +1,9 @@
 class CreateLists < ActiveRecord::Migration[5.2]
   def change
     create_table :lists do |t|
-
+      #カラムの定義
+          t.string :title
+          t.string :body
       t.timestamps
     end
   end

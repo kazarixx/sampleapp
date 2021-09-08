22c66961818eb45185f53ffc1c2d3b51e7b0729b
kazarixx <tua.ih8@gmail.com>
2021-09-08 17:12:08 +0000

    [middle]link_to

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -17,6 +17,10 @@ class TodolistsController < ApplicationController
   def index
     @lists = List.all
   end
+  
+  def show
+    @list = List.find(params[:id])
+  end
 
   private
   # ストロングパラメータ

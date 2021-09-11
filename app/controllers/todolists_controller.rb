7af25f0e2027ad742351df7354ff0bbae685f732
kazarixx <tua.ih8@gmail.com>
2021-09-11 13:21:08 +0000

    [middle]rails9章

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -31,10 +31,17 @@ class TodolistsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
+  
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
+  end
+  
+   def destroy
+     list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 
 end
